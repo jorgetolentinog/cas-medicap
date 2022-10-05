12528edd2a05d801adf35b2e88cfcd0b0ee0e47a
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-05 17:12:23 -0500

    -

--- cas-medicap-sync/src/function/http/sync-post.ts
+++ cas-medicap-sync/src/function/http/sync-post.ts
@@ -1,12 +1,6 @@
-import "reflect-metadata";
-
 import { APIGatewayEvent } from "aws-lambda";
-import { container } from "tsyringe";
-import { SyncPostController } from "./controller/SyncPostController";
 
 export const handler = async (event: APIGatewayEvent) => {
-  await container.resolve(SyncPostController).execute(event);
-
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "hello from Sync TSx" }),
