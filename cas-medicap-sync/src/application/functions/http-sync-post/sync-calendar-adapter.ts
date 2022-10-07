d24e33fdaa2e167e8b77181727a56717acc9b178
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-07 09:32:34 -0500

    -

---
+++ cas-medicap-sync/src/application/functions/http-sync-post/sync-calendar-adapter.ts
@@ -0,0 +1,109 @@
+import { z } from "zod";
+import { APIGatewayEvent } from "aws-lambda";
+import { localDate } from "@/domain/service/date";
+import { container } from "tsyringe";
+import { SyncCalendar } from "@/domain/usecase/sync-calendar/SyncCalendar";
+
+export const syncCalendarAdapter = async (event: APIGatewayEvent) => {
+  const body = bodyParser(event.body ?? "");
+
+  if (!body.success) {
+    throw new Error("Invalid request body");
+  }
+
+  const startDate = localDate.parse(
+    body.data.data.desde,
+    "DD/MM/YYYY",
+    "YYYY-MM-DD"
+  );
+
+  const endDate = localDate.parse(
+    body.data.data.hasta,
+    "DD/MM/YYYY",
+    "YYYY-MM-DD"
+  );
+
+  const minAge = Number(
+    body.data.data.condicionesAtencionServicio.edadMinimaAtencion
+  );
+  const maxAge = Number(
+    body.data.data.condicionesAtencionServicio.edadMinimaAtencion
+  );
+  const gender = genderParser(
+    body.data.data.condicionesAtencionServicio.generoAtencion
+  );
+
+  await container.resolve(SyncCalendar).execute({
+    id: body.data.data.indice,
+    startDate: startDate,
+    endDate: endDate,
+    isEnabled: body.data.data.vigencia,
+    companyId: body.data.data.codigoEmpresa,
+    officeId: body.data.data.codigoSucursal,
+    serviceId: body.data.data.codigoServicio,
+    medicalAreaIds: body.data.data.codigosAreasMedicas,
+    interestAreaIds: body.data.data.codigosAreasDeInteres,
+    professionalId: body.data.data.ppnProfesional,
+    blockDurationInMinutes: body.data.data.duracionBloques,
+    conditionsOfService: {
+      minAge: minAge,
+      maxAge: maxAge,
+      gender: gender,
+    },
+    days: body.data.data.dias.map((dia) => {
+      return {
+        dayOfWeek: Number(dia.diaSemana),
+        blocks: dia.bloques.map((bloque) => {
+          return {
+            startTime: bloque[0].padStart(5, "0") + ":00",
+            endTime: bloque[1].padStart(5, "0") + ":00",
+          };
+        }),
+      };
+    }),
+  });
+};
+
+function genderParser(gender: string) {
+  if (gender !== "F" && gender !== "M") {
+    return undefined;
+  }
+  return gender;
+}
+
+function bodyParser(body: string) {
+  const stringify = z
+    .string()
+    .or(z.number())
+    .transform((value) => value.toString());
+
+  const schema = z.object({
+    type: z.literal("RSV"),
+    data: z.object({
+      indice: stringify,
+      desde: z.string(),
+      hasta: z.string(),
+      codigoEmpresa: stringify,
+      codigoSucursal: stringify,
+      codigoServicio: stringify,
+      ppnProfesional: stringify,
+      vigencia: z.boolean(),
+      codigosAreasMedicas: z.array(z.string()),
+      codigosAreasDeInteres: z.array(z.string()),
+      duracionBloques: z.number(),
+      condicionesAtencionServicio: z.object({
+        edadMaximaAtencion: stringify,
+        edadMinimaAtencion: stringify,
+        generoAtencion: z.string(),
+      }),
+      dias: z.array(
+        z.object({
+          diaSemana: z.string(),
+          bloques: z.array(z.array(z.string())),
+        })
+      ),
+    }),
+  });
+
+  return schema.safeParse(JSON.parse(body));
+}
