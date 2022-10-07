9392f9bd61080ddbfe2a62dc199da8795f9832d6
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-07 10:36:52 -0500

    -

--- cas-medicap-sync/src/infrastructure/repository/dynamodb/adapter/DynamoDBReleaseRepository.ts
+++ cas-medicap-sync/src/infrastructure/repository/dynamodb/adapter/DynamoDBReleaseRepository.ts
@@ -1,16 +1,97 @@
 import { injectable } from "tsyringe";
 import { ReleaseRepository } from "@/domain/repository/ReleaseRepository";
 import { Release } from "@/domain/schema/Release";
+import { DynamoDB } from "../DynamoDB";
 
 @injectable()
 export class DynamoDBReleaseRepository implements ReleaseRepository {
-  create(release: Release): Promise<void> {
-    throw new Error("Method not implemented.");
+  private _table = process.env.DYNAMODB_TABLE_RELEASE ?? 'ReleaseTable'
+
+  constructor(private dynamodb: DynamoDB) {}
+
+  async create(release: Release): Promise<void> {
+    await this.dynamodb.client
+      .put({
+        TableName: this._table,
+        Item: {
+          id: release.id,
+          date: release.date,
+          blockDurationInMinutes: release.blockDurationInMinutes,
+          professionalId: release.professionalId,
+          serviceId: release.serviceId,
+          isEnabled: release.isEnabled,
+          createdAt: release.createdAt,
+          updatedAt: release.updatedAt
+        },
+        ConditionExpression: 'attribute_not_exists(id)'
+      })
+      .promise()
   }
-  update(release: Release): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async update(release: Release): Promise<void> {
+    const attrs = {
+      date: release.date,
+      blockDurationInMinutes: release.blockDurationInMinutes,
+      professionalId: release.professionalId,
+      serviceId: release.serviceId,
+      isEnabled: release.isEnabled,
+      createdAt: release.createdAt,
+      updatedAt: release.updatedAt
+    }
+
+    let updateExpression = 'set '
+    const expressionAttributeNames: Record<string, string> = {}
+    const expressionAttributeValues: Record<string, unknown> = {}
+
+    for (const prop in attrs) {
+      updateExpression += `#${prop} = :${prop},`
+      expressionAttributeNames[`#${prop}`] = prop
+      expressionAttributeValues[`:${prop}`] = (attrs as Record<string, unknown>)[
+        prop
+      ]
+    }
+    updateExpression = updateExpression.slice(0, -1)
+
+    await this.dynamodb.client
+      .update({
+        TableName: this._table,
+        Key: {
+          id: release.id
+        },
+        UpdateExpression: updateExpression,
+        ConditionExpression: 'attribute_exists(id)',
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues
+      })
+      .promise()
   }
-  findById(releaseId: string): Promise<Release | null> {
-    throw new Error("Method not implemented.");
+
+  async findById(releaseId: string): Promise<Release | null> {
+    const result = await this.dynamodb.client
+      .query({
+        TableName: this._table,
+        KeyConditionExpression: '#id = :id',
+        ExpressionAttributeNames: { '#id': 'id' },
+        ExpressionAttributeValues: {
+          ':id': releaseId
+        }
+      })
+      .promise()
+
+    const item = result.Items && result.Items[0]
+    if (item == null) {
+      return null
+    }
+
+    return {
+      id: item.id,
+      date: item.date,
+      blockDurationInMinutes: item.blockDurationInMinutes,
+      professionalId: item.professionalId,
+      serviceId: item.serviceId,
+      isEnabled: item.isEnabled,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt
+    }
   }
 }
