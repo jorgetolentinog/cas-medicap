4bab0a285e338dc2c93859351d6914eebe807cb2
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-07 10:46:04 -0500

    -

--- cas-medicap-sync/src/infrastructure/repository/dynamodb/adapter/DynamoDBCalendarRepository.ts
+++ cas-medicap-sync/src/infrastructure/repository/dynamodb/adapter/DynamoDBCalendarRepository.ts
@@ -1,16 +1,117 @@
 import { injectable } from "tsyringe";
 import { CalendarRepository } from "@/domain/repository/CalendarRepository";
 import { Calendar } from "@/domain/schema/Calendar";
+import { DynamoDB } from "../DynamoDB";
 
 @injectable()
 export class DynamoDBCalendarRepository implements CalendarRepository {
-  create(calendar: Calendar): Promise<void> {
-    throw new Error("Method not implemented.");
+  private _table = process.env.DYNAMODB_TABLE_CALENDAR ?? "CalendarTable";
+
+  constructor(private dynamodb: DynamoDB) {}
+
+  async create(calendar: Calendar): Promise<void> {
+    await this.dynamodb.client
+      .put({
+        TableName: this._table,
+        Item: {
+          id: calendar.id,
+          startDate: calendar.startDate,
+          endDate: calendar.endDate,
+          isEnabled: calendar.isEnabled,
+          companyId: calendar.companyId,
+          officeId: calendar.officeId,
+          serviceId: calendar.serviceId,
+          medicalAreaIds: calendar.medicalAreaIds,
+          interestAreaIds: calendar.interestAreaIds,
+          professionalId: calendar.professionalId,
+          blockDurationInMinutes: calendar.blockDurationInMinutes,
+          conditionsOfService: calendar.conditionsOfService,
+          days: calendar.days,
+          createdAt: calendar.createdAt,
+          updatedAt: calendar.updatedAt,
+        },
+        ConditionExpression: "attribute_not_exists(id)",
+      })
+      .promise();
   }
-  update(calendar: Calendar): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async update(calendar: Calendar): Promise<void> {
+    const attrs = {
+      startDate: calendar.startDate,
+      endDate: calendar.endDate,
+      isEnabled: calendar.isEnabled,
+      companyId: calendar.companyId,
+      officeId: calendar.officeId,
+      serviceId: calendar.serviceId,
+      medicalAreaIds: calendar.medicalAreaIds,
+      interestAreaIds: calendar.interestAreaIds,
+      professionalId: calendar.professionalId,
+      blockDurationInMinutes: calendar.blockDurationInMinutes,
+      conditionsOfService: calendar.conditionsOfService,
+      days: calendar.days,
+      createdAt: calendar.createdAt,
+      updatedAt: calendar.updatedAt,
+    };
+
+    let updateExpression = "set ";
+    const expressionAttributeNames: Record<string, string> = {};
+    const expressionAttributeValues: Record<string, unknown> = {};
+    for (const prop in attrs) {
+      updateExpression += `#${prop} = :${prop},`;
+      expressionAttributeNames[`#${prop}`] = prop;
+      expressionAttributeValues[`:${prop}`] = (
+        attrs as Record<string, unknown>
+      )[prop];
+    }
+    updateExpression = updateExpression.slice(0, -1);
+
+    await this.dynamodb.client
+      .update({
+        TableName: this._table,
+        Key: {
+          id: calendar.id,
+        },
+        UpdateExpression: updateExpression,
+        ConditionExpression: "attribute_exists(id)",
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues,
+      })
+      .promise();
   }
-  findById(calendarId: string): Promise<Calendar | null> {
-    throw new Error("Method not implemented.");
+
+  async findById(calendarId: string): Promise<Calendar | null> {
+    const result = await this.dynamodb.client
+      .query({
+        TableName: this._table,
+        KeyConditionExpression: "#id = :id",
+        ExpressionAttributeNames: { "#id": "id" },
+        ExpressionAttributeValues: {
+          ":id": calendarId,
+        },
+      })
+      .promise();
+
+    const item = result.Items && result.Items[0];
+    if (item == null) {
+      return null;
+    }
+
+    return {
+      id: item.id,
+      startDate: item.startDate,
+      endDate: item.endDate,
+      isEnabled: item.isEnabled,
+      companyId: item.companyId,
+      officeId: item.officeId,
+      serviceId: item.serviceId,
+      medicalAreaIds: item.medicalAreaIds,
+      interestAreaIds: item.interestAreaIds,
+      professionalId: item.professionalId,
+      blockDurationInMinutes: item.blockDurationInMinutes,
+      conditionsOfService: item.conditionsOfService,
+      days: item.days,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt,
+    };
   }
 }
--- cas-medicap-sync/src/infrastructure/repository/dynamodb/adapter/DynamoDBExceptionRepository.ts
+++ cas-medicap-sync/src/infrastructure/repository/dynamodb/adapter/DynamoDBExceptionRepository.ts
@@ -1,16 +1,114 @@
 import { injectable } from "tsyringe";
 import { ExceptionRepository } from "@/domain/repository/ExceptionRepository";
 import { Exception } from "@/domain/schema/Exception";
+import { DynamoDB } from "../DynamoDB";
 
 @injectable()
 export class DynamoDBExceptionRepository implements ExceptionRepository {
-  create(exception: Exception): Promise<void> {
-    throw new Error("Method not implemented.");
+  private _table = process.env.DYNAMODB_TABLE_EXCEPTION ?? "ExceptionTable";
+
+  constructor(private dynamodb: DynamoDB) {}
+
+  async create(exception: Exception): Promise<void> {
+    await this.dynamodb.client
+      .put({
+        TableName: this._table,
+        Item: {
+          id: exception.id,
+          startDate: exception.startDate,
+          endDate: exception.endDate,
+          isEnabled: exception.isEnabled,
+          recurrence: exception.recurrence,
+          repeatRecurrenceEvery: exception.repeatRecurrenceEvery,
+          professionalIds: exception.professionalIds,
+          serviceIds: exception.serviceIds,
+          dayOfMonth: exception.dayOfMonth,
+          weekOfMonth: exception.weekOfMonth,
+          dayOfWeek: exception.dayOfWeek,
+          days: exception.days,
+          createdAt: exception.createdAt,
+          updatedAt: exception.updatedAt,
+        },
+        ConditionExpression: "attribute_not_exists(id)",
+      })
+      .promise();
   }
-  update(exception: Exception): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async update(exception: Exception): Promise<void> {
+    const attrs = {
+      startDate: exception.startDate,
+      endDate: exception.endDate,
+      isEnabled: exception.isEnabled,
+      recurrence: exception.recurrence,
+      repeatRecurrenceEvery: exception.repeatRecurrenceEvery,
+      professionalIds: exception.professionalIds,
+      serviceIds: exception.serviceIds,
+      dayOfMonth: exception.dayOfMonth,
+      weekOfMonth: exception.weekOfMonth,
+      dayOfWeek: exception.dayOfWeek,
+      days: exception.days,
+      createdAt: exception.createdAt,
+      updatedAt: exception.updatedAt,
+    };
+
+    let updateExpression = "set ";
+    const expressionAttributeNames: Record<string, string> = {};
+    const expressionAttributeValues: Record<string, unknown> = {};
+    for (const prop in attrs) {
+      updateExpression += `#${prop} = :${prop},`;
+      expressionAttributeNames[`#${prop}`] = prop;
+      expressionAttributeValues[`:${prop}`] = (
+        attrs as unknown as Record<string, unknown>
+      )[prop];
+    }
+    updateExpression = updateExpression.slice(0, -1);
+
+    await this.dynamodb.client
+      .update({
+        TableName: this._table,
+        Key: {
+          id: exception.id,
+        },
+        UpdateExpression: updateExpression,
+        ConditionExpression: "attribute_exists(id)",
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues,
+      })
+      .promise();
   }
-  findById(exceptionId: string): Promise<Exception | null> {
-    throw new Error("Method not implemented.");
+
+  async findById(exceptionId: string): Promise<Exception | null> {
+    const result = await this.dynamodb.client
+      .query({
+        TableName: this._table,
+        KeyConditionExpression: "#id = :id",
+        ExpressionAttributeNames: { "#id": "id" },
+        ExpressionAttributeValues: {
+          ":id": exceptionId,
+        },
+      })
+      .promise();
+
+    const item = result.Items && result.Items[0];
+    if (item == null) {
+      return null;
+    }
+
+    return {
+      id: item.id,
+      startDate: item.startDate,
+      endDate: item.endDate,
+      isEnabled: item.isEnabled,
+      recurrence: item.recurrence,
+      repeatRecurrenceEvery: item.repeatRecurrenceEvery,
+      professionalIds: item.professionalIds,
+      serviceIds: item.serviceIds,
+      dayOfMonth: item.dayOfMonth,
+      weekOfMonth: item.weekOfMonth,
+      dayOfWeek: item.dayOfWeek,
+      days: item.days,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt,
+    };
   }
 }
