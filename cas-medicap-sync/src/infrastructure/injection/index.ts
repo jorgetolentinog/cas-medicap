3f5c25761622af4de2615808e51d7407c93c9d87
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-07 08:54:55 -0500

    -

--- cas-medicap-sync/src/infrastructure/injection/index.ts
+++ cas-medicap-sync/src/infrastructure/injection/index.ts
@@ -1,10 +1,38 @@
 import { container } from "tsyringe";
 import { BookingRepository } from "@/domain/repository/BookingRepository";
 import { DynamoDBBookingRepository } from "@/infrastructure/repository/DynamoDBBookingRepository";
+import { PreBookingRepository } from "@/domain/repository/PreBookingRepository";
+import { DynamoDBPreBookingRepository } from "@/infrastructure/repository/DynamoDBPreBookingRepository";
+import { ReleaseRepository } from "@/domain/repository/ReleaseRepository";
+import { DynamoDBReleaseRepository } from "@/infrastructure/repository/DynamoDBReleaseRepository";
+import { CalendarRepository } from "@/domain/repository/CalendarRepository";
+import { DynamoDBCalendarRepository } from "@/infrastructure/repository/DynamoDBCalendarRepository";
+import { ExceptionRepository } from "@/domain/repository/ExceptionRepository";
+import { DynamoDBExceptionRepository } from "@/infrastructure/repository/DynamoDBExceptionRepository";
 
 container.register<BookingRepository>(
   "BookingRepository",
   DynamoDBBookingRepository
+);
+
+container.register<PreBookingRepository>(
+  "PreBookingRepository",
+  DynamoDBPreBookingRepository
+);
+
+container.register<ReleaseRepository>(
+  "ReleaseRepository",
+  DynamoDBReleaseRepository
+);
+
+container.register<CalendarRepository>(
+  "CalendarRepository",
+  DynamoDBCalendarRepository
+);
+
+container.register<ExceptionRepository>(
+  "ExceptionRepository",
+  DynamoDBExceptionRepository
 );
 
 export { container };
